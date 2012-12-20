73842688c34687bae4d63cec43d99d45c12f7c29
icwsee <icwsee@gmail.com>
2012-12-20 19:00:29 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRubyApp::Application.config.secret_token = '69382c81dc3703cf8a4e863d13cb69aec8bdfd8d2c252b6d163a559c30572208976048ad2de03eb327ab82f217c366ef19bdbd037e98948bc07f36ec6da89c51'
